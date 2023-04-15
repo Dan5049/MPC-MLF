c6101c2a76fc6f13f70edd0bc7d9404c9841e2c8
Dan5049 <havranek.d@hotmail.cz>
2023-04-15 13:17:28 +0200

    data extraction - fix

--- Final project/data.py
+++ Final project/data.py
@@ -2,11 +2,21 @@ import numpy as np
 import pandas as pd
 import glob
 
-path_test = glob.glob("C:/Users/Dan/Documents/MLF/Dataset/Test/CSV/*.csv")
-path_train = glob.glob("C:/Users/Dan/Documents/MLF/Dataset/Train/CSV/*.csv")
+file_test = glob.glob("C:/Users/Dan/Documents/MLF/Dataset/Test/CSV/*.csv")
+file_train = glob.glob("C:/Users/Dan/Documents/MLF/Dataset/Train/CSV/*.csv")
+num_test = len(file_test)
+num_train = len(file_train)
 
+path_test = []
+path_train = []
 csv_test = []
 csv_train = []
+
+for i in range(num_test):
+  path_test.append("C:/Users/Dan/Documents/MLF/Dataset/Test/CSV/img_%d.csv" %(i))
+
+for j in range(num_train):
+  path_train.append("C:/Users/Dan/Documents/MLF/Dataset/Train/CSV/img_%d.csv" %(j))
 
 print("loading")
 
